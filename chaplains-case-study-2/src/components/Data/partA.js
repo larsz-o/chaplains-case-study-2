9fe907eeda03137f27da0857a040b42dfd6ea6a5
Lars Mackenzie <larsmackenzie@larss-air.mynetworksettings.com>
2024-11-18 21:04:31 -0500

    content

--- chaplains-case-study-2/src/components/Data/partA.js
+++ chaplains-case-study-2/src/components/Data/partA.js
@@ -7,7 +7,7 @@ const partA = (
 <p>How should you respond?</p>`,
         options: [
           { text: `"No, I don’t think we should. He killed two people, and himself. He is the source of violence in this situation, and it feels disrespectful to the victims and their families to remember him in this way.”`, conditions: {type: null, command: "NEXT", index: null}, feedback: `This is a very difficult question to answer, but this isn’t the best choice. As a chaplain, it is your responsibility to hold difficult truths. Mario was a husband, father, colleague, and member of the community. While no one supports his actions, you can choose to embrace the healing power of forgiveness by holding a memorial service for Mario.` },
-          { text: `Yes, we were asked to plan the service, and it’s our duty to serve all people, even those who commit acts of violence.”`, conditions: {type: null, command: "NEXT", index: null}, feedback: `This is a very difficult question to answer, but this is the best choice. Mario was a husband, father, colleague, and member of the community. While no one supports his actions, you can choose to respond much as defense attorneys respond, with a commitment to the humanity of every individual.` },
+          { text: `"Yes, we were asked to plan the service, and it’s our duty to serve all people, even those who commit acts of violence.”`, conditions: {type: null, command: "NEXT", index: null}, feedback: `This is a very difficult question to answer, but this is the best choice. Mario was a husband, father, colleague, and member of the community. While no one supports his actions, you can choose to respond much as defense attorneys respond, with a commitment to the humanity of every individual.` },
           
         ],
         multipleSelect: false,
@@ -21,19 +21,19 @@ const partA = (
         <p>Which one would you choose?</p>
         `, 
         options: [
-          { text: `Hold a memorial service at the hospital, in a worship space. A small group of senior hospital leaders and staff have asked that Mario’s memorial service be held at the hospital in the worship space. They believe that each person who died that day should be remembered.”`, conditions: {type: null, command: "NEXT", index: null}, feedback: `This isn't a bad choice, but there is a better one. Consider another option.` },
-          { text: `Hold a memorial service off campus at a nearby church. Some senior hospital leaders and staff have asked that the memorial service be held off-site at a nearby church, so that anyone who wants to avoid the service doesn’t have to pass it during their work day.`, conditions: {type: null, command: "NEXT", index: null}, feedback: `This choice balances the needs of most of the stakeholders by holding a service for Mario, but not bringing that service into the space of the hospital. This choice avoids retraumatizing members of the hospital community, while still holding firm in the hospital’s values of “caring for the people in our community at all stages of life, from beginning to end.”` },
-          { text: `Don’t hold a memorial service for Mario at all. Several senior hospital leaders and staff are adamantly opposed to holding a memorial service for Mario that’s affiliated with the hospital at all. They don’t want to give him any notoriety for this tragedy.`, conditions: {type: null, command: "NEXT", index: null}, feedback: `This is not the best choice. Consider another option.` },
-          { text: `Hold a memorial service at the hospital, at the site of the shooting. A small group of hospital staff have voiced their desire to hold Mario’s memorial service at the site of the shooting, to hold the pain of the violence that all suffered on that day.`, conditions: {type: null, command: "NEXT", index: null}, feedback: `This is not the best choice. Consider another option.` },
+          { text: `Hold a memorial service at the hospital, in a worship space.”`, conditions: {type: null, command: "NEXT", index: null}, feedback: `A small group of senior hospital leaders and staff have asked that Mario’s memorial service be held at the hospital in the worship space. They believe that each person who died that day should be remembered. This isn't a bad choice, but there is a better one. Consider another option.` },
+          { text: `Hold a memorial service off campus at a nearby church.`, conditions: {type: null, command: "NEXT", index: null}, feedback: `Some senior hospital leaders and staff have asked that the memorial service be held off-site at a nearby church, so that anyone who wants to avoid the service doesn’t have to pass it during their work day. This choice balances the needs of most of the stakeholders by holding a service for Mario, but not bringing that service into the space of the hospital. This choice avoids retraumatizing members of the hospital community, while still holding firm in the hospital’s values of “caring for the people in our community at all stages of life, from beginning to end.”` },
+          { text: `Don’t hold a memorial service for Mario at all.`, conditions: {type: null, command: "NEXT", index: null}, feedback: `Several senior hospital leaders and staff are adamantly opposed to holding a memorial service for Mario that’s affiliated with the hospital at all. They don’t want to give him any notoriety for this tragedy.This is not the best choice. Consider another option.` },
+          { text: `Hold a memorial service at the hospital, at the site of the shooting. `, conditions: {type: null, command: "NEXT", index: null}, feedback: `A small group of hospital staff have voiced their desire to hold Mario’s memorial service at the site of the shooting, to hold the pain of the violence that all suffered on that day. This is not the best choice. Consider another option.` }
             
         ],
         multipleSelect: false,
-        type: "open", 
+        type: "choice", 
         popUp: false, 
       popUpContent: ``
     }, 
     "3": {
-        question: `<p>The final decision was for the hospital to host all three memorial services. Those for Hugo and Kelly were held on campus. The service for Mario took place off campus, away from the hospital, at a local church. You attended and spoke at his service. </p>`,
+        question: `<p>The final decision was for the hospital to host all three memorial services. Those for Hugo and Kelly were held on campus. The service for Mario took place off campus, away from the hospital, at a local church. You attended and spoke at his service. </p><p>Dr. Hendel attended the family funerals for Hugo and Kelly as well as the memorial services on campus. She delivered eulogies at both of the on campus memorial services by invitation. Dr. Hendel decided not to attend Mario’s service off campus.</p><p>As she writes, “I decided that going would cross the line, integrity-wise, for me, personally, and that, as CEO, foregoing the service was also best for the whole. Instead, I wrote a brief statement on behalf of the hospital administration expressing condolences to his family and gave it to the chaplain to read at the service. A service that ended up being attended by several hundred people.”</p>`,
         options: [ 
            
             {text: `I promise I will not tell anyone. You can always talk to me.`, conditions: {type: null, command: "NEXT", index: null}, feedback: `While this isn't the worst choice, you should consider the complexity of the situation before promising Sergeant Ruiz that you will never tell anyone else about what she shared. You can help her access resources, and you should let her know that you can be a supportive person to her through this process.`},
@@ -42,9 +42,9 @@ const partA = (
            
         ], 
         multipleSelect: false, 
-        type: "choice", 
+        type: "none", 
         popUp: true, 
-        popUpContent: `<p>For more on chaplain confidentiality in the military, see Rule 503 of the Uniform Code of Military Justice.  For discussion related specifically to confidentiality in U.S. Army chaplaincy, see paragraph 16-2 in Army  Regulation 165-1, Army Chaplain Corps Activities (5 March 2024).</p><p>Learn more about <a href="https://sapr.mil/reporting-options" target="_blank" rel="noopener noreferrer">reporting sexual assault in the military</a>.</p>`
+        popUpContent: `<p>How do you feel about the decisions that were made? Would you have chosen differently if you had the chance? Whose voices were most important to honor in this situation?</p>`
     },
     "4": {
       question: `<p>"I can keep this confidential for you, but I can also help you share this with some other helpers. I understand your desire for privacy, Sergeant Ruiz," you say, "and I can't begin to imagine what  you are feeling right now. Thank you for coming and sharing this with me. I feel so privileged to sit with you,  even here in the midst of confusion and pain. I want to help you work through what happened to you – both  right now and in the days to come. I also want you to know that I will be limited in the help that I can give you  if we don't share what happened to you with other helpers." </p><p>"What do you mean, 'other helpers'?" she asks.</p>
